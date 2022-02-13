a0e5b81b65df483a45fedef3c9e9deeceb2ea20c
zhhf666666 <1056743413@qq.com>
2022-02-13 20:15:03 +0000

    添加重力&跳跃

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,11 @@ public class PlayerController : MonoBehaviour
     public float VerticalOffsetLimitation = 60;
     public CharacterController CC;
     public float MoveSpeed = 10;
+    public float G = 9.8f;
+    public float FallVelocity = 0;
+    public Transform CheckPoint;
+    public float magrin = 0.1f;
+    public float JumpSpeed = 5.0f;
 
     void Start()
     {
@@ -33,7 +38,7 @@ public class PlayerController : MonoBehaviour
 
     private void PlayerRotateControl()
     {
-        if(PlayerTrans==null || EyeTrans==null)
+        if(PlayerTrans == null || EyeTrans == null)
             return;
         float offset_x = Input.GetAxis("Mouse X");   // Horizontal
         float offset_y = Input.GetAxis("Mouse Y");   // Vertical
@@ -43,15 +48,38 @@ public class PlayerController : MonoBehaviour
         EyeTrans.localRotation = Quaternion.Euler(new Vector3(VerticalOffset, EyeTrans.localEulerAngles.y, EyeTrans.localEulerAngles.z));
     }
 
+    public bool IsOnGround()
+    {
+        if(CheckPoint == null)
+            return false;
+        return Physics.Raycast(CheckPoint.position, Vector3.down, magrin);
+    }
+
     public void PlayerMove()
     {
-        if(CC==null)
+        if(CC == null)
             return;
         Vector3 MoveValue = Vector3.zero;
         float LeftRightValue = Input.GetAxis("Horizontal");
         float FrontBackValue = Input.GetAxis("Vertical");
         MoveValue += this.transform.forward * MoveSpeed * FrontBackValue * Time.deltaTime;
         MoveValue += this.transform.right * MoveSpeed * LeftRightValue * Time.deltaTime;
+
+        // Fall
+        if(IsOnGround() == false)
+        {
+            FallVelocity += G * Time.deltaTime;
+        }
+        else
+        {
+            FallVelocity = 0;
+            if(Input.GetButtonDown("Jump"))
+            {
+                FallVelocity = -JumpSpeed;
+            }
+        }
+        MoveValue += Vector3.down * FallVelocity * Time.deltaTime;
+
         CC.Move(MoveValue);
     }
 }
